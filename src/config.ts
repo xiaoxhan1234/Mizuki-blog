8e80d29c5a3993ed5ed4877fe4ac954352ecf86e
XiaoXhan1234 <3084058513@qq.com>
2025-10-25 19:48:37 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -302,7 +302,7 @@ export const expressiveCodeConfig: ExpressiveCodeConfig = {
 export const commentConfig: CommentConfig = {
 	enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
 	twikoo: {
-		envId: "https://twikoo.vercel.app",
+		envId: "https://twikoo.zako.wf",
 		lang: "zh_CN", // 设置 Twikoo 评论系统语言为英文
 	},
 };
